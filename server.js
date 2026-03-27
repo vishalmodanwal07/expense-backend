d75f93525aa41ade4cc9b39941daf0abb3342367
vishal modanwal <vk0764394@gmail.com>
2026-03-27 14:23:30 +0530

    add config middlewares

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ config({ path: "./.env" });
 dbConnect();
 
 app.listen(process.env.PORT , ()=>{
-    console.log(" app is up and running on port 3000");
+    console.log(`app is up and running on port ${process.env.PORT}`);
 })
 
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import {config} from "dotenv";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/v1/health', (req, res) => res.json({ status: 'ok' }));
 
 
 
